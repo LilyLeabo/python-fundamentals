6e8fc598c589194904ed36d862bdcab8b7da2be2
Leabs <stevenleabo@gmail.com>
2023-02-03 14:12:54 -0500

    IF statement examples

---
+++ 01-if/if.py
@@ -0,0 +1,17 @@
+# Python file that explains IF statements
+
+# Set variable animal to "cat"
+animal = "cat"
+
+# If animal is "cat" then print "Meow"
+if animal == "cat":
+    print("Meow")
+
+# If animal is "dog" then print "Woof"
+if animal == "dog":
+    print("Woof")
+
+# If animal is "cow" then print "Moo"
+if animal == "cow":
+    print("Moo")
+
